dd8e7ee60dd90d315c0f77c1a3fa142496d0f733
ziadhaidar <zizoohaidar84@gmail.Com>
2025-05-22 17:06:34 +0200

    Update tanzil.js

--- src/services/tanzil.js
+++ src/services/tanzil.js
@@ -1,7 +1,13 @@
-// src/services/tanzil.js
-export async function fetchQuran() {
-  const res = await fetch('https://tanzil.net/trans/13113/json');
-  if (!res.ok) throw new Error('Failed to load Quran data');
-  return await res.json();
-}
+// src/services/tanzil.ts
+export type Chapter = {
+  chapter: number;
+  verses: string[];
+};
 
+export async function fetchQuran(): Promise<Chapter[]> {
+  const res = await fetch('https://tanzil.net/trans/13113/json');
+  if (!res.ok) {
+    throw new Error('Failed to load Quran data');
+  }
+  return (await res.json()) as Chapter[];
+}
