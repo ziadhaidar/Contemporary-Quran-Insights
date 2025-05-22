b8bbfcd21d27aecdeac7e33c6a901e6274250470
ziadhaidar <zizoohaidar84@gmail.Com>
2025-05-22 17:02:50 +0200

    Update tanzil.js

--- src/services/tanzil.js
+++ src/services/tanzil.js
@@ -1,1 +1,7 @@
+// src/services/tanzil.js
+export async function fetchQuran() {
+  const res = await fetch('https://tanzil.net/trans/13113/json');
+  if (!res.ok) throw new Error('Failed to load Quran data');
+  return await res.json();
+}
 
